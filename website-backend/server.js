04b09bb1ad6cb23369ef36f07fd5b15134e90582
Philip <philipandersson27@gmail.com>
2023-08-13 21:47:47 +0200

    Add a little mongoose code to node.js server.

--- website-backend/server.js
+++ website-backend/server.js
@@ -1,8 +1,11 @@
 // Import the Express library.
 const express = require('express');
-
 // Initializing the app.
 const app = express();
+
+// Import mongoose and connect to MongoDB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/brilliantwear');
 
 // Getting the path request and sending the response with text
 app.get('/', (req, res) => {
