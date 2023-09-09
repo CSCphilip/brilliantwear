6c294feb30402635434be97db2c7b1568199750f
Philip <philipandersson27@gmail.com>
2023-09-09 20:48:40 +0200

    Add dropdown in navbar for login.

--- website/react-vite/react-app/src/pages/Layout.tsx
+++ website/react-vite/react-app/src/pages/Layout.tsx
@@ -1,5 +1,5 @@
 import Favicon from "react-favicon";
-import { Outlet, Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
@@ -58,6 +58,63 @@ const Layout = () => {
                 </ul>
               </li>
             </ul>
+          </div>
+
+          <div className="dropdown">
+            <button
+              type="button"
+              className="btn btn-primary dropdown-toggle"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              data-bs-auto-close="outside"
+            >
+              Log In
+            </button>
+            <form className="dropdown-menu p-4 dropdown-menu-end custom-dropdown-width">
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleDropdownFormEmail2"
+                  className="form-label"
+                >
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleDropdownFormEmail2"
+                  placeholder="email@example.com"
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleDropdownFormPassword2"
+                  className="form-label"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleDropdownFormPassword2"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="mb-3">
+                <div className="form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="dropdownCheck2"
+                  />
+                  <label className="form-check-label" htmlFor="dropdownCheck2">
+                    Remember me
+                  </label>
+                </div>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Sign in
+              </button>
+            </form>
           </div>
         </div>
       </nav>
