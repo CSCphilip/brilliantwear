e15b3f1ecb08f9c5b05d4fe2eb5a2e4523db644c
Philip <philipandersson27@gmail.com>
2023-09-17 21:43:53 +0200

    Improve the handling of jwt secret key. Now it is cached after being fetched once.

--- backend/config/auth.config.js
+++ backend/config/auth.config.js
@@ -4,14 +4,23 @@ AWS.config.update({ region: "eu-north-1" });
 const ssm = new AWS.SSM();
 const parameterName = "Brilliantwear-JWT-Secret-Key";
 
+let cachedJWTSecretKey = null; // To cache the JWT secret key
+
 async function getJWTSecretKey() {
+  if (cachedJWTSecretKey !== null) {
+    // If the secret key is already cached, return it immediately
+    return cachedJWTSecretKey;
+  }
+
   try {
     console.log("Trying to retrieve the JWT Secret key from AWS");
 
     const data = await ssm.getParameter({ Name: parameterName }).promise();
     console.log("Successfully retrieved the JWT Secret key from AWS");
 
-    return data.Parameter.Value;
+    // Cache the secret key and return it
+    cachedJWTSecretKey = data.Parameter.Value;
+    return cachedJWTSecretKey;
   } catch (err) {
     console.log("Could not retrieve the JWT Secret key from AWS");
     console.error("Error:", err);
@@ -26,6 +35,8 @@ async function getJWTSecretKey() {
       "Successfully retrieved the JWT Secret key from the environment"
     );
 
+    // Cache the secret key and return it
+    cachedJWTSecretKey = jwtSecretKey;
     return jwtSecretKey;
   } catch (err) {
     console.log("Could not retrieve the JWT Secret key from the environment");
