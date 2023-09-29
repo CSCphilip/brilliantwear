eb61ccb8cda8febf15d513194ab7c673216508f0
Philip Andersson <philipandersson27@gmail.com>
2023-09-29 19:25:49 +0000

    Made a small change to mongodb connect for it to work when using containers in docker.

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  HOST: "0.0.0.0", // This should be localhost if it doesn't mean the same
+  // In dev:   HOST: "0.0.0.0", // This should be localhost if it doesn't mean the same
+  HOST: "mongoservice", // Used in container
   PORT: 27017,
   DB: "brilliantwear",
 };
