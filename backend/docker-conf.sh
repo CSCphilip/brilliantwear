87cf4604d2cde7a961bd438109cdcb4150837203
Philip Andersson <philipandersson27@gmail.com>
2023-09-30 09:36:53 +0000

    Small changes to the docker-conf file.

--- backend/docker-conf.sh
+++ backend/docker-conf.sh
@@ -1,3 +1,5 @@
 docker build -t cscphilip/brilliantwear-backend .
-docker run -p 7000:7000 --add-host=mongoservice:172.17.0.1 -d cscphilip/brilliantwear-backend # To create one container
-docker stack deploy --compose-file brilliantwear-backend-docker-compose-file.yml bw-stack # To create a stack of containers
+# To create one container:
+docker run -p 7000:7000 --add-host=mongoservice:172.17.0.1 -d cscphilip/brilliantwear-backend
+# To create a stack of containers:
+docker stack deploy --compose-file brilliantwear-backend-docker-compose-file.yml bw-stack
