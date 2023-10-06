2cfda483c8c69f2b6201abccdb18b6c9df9952fe
Philip Andersson <philipandersson27@gmail.com>
2023-10-06 21:42:04 +0000

    A name change of script.

--- backend/start-backend-docker.sh
+++ backend/start-backend-docker.sh
@@ -3,6 +3,5 @@
 docker build -t cscphilip/brilliantwear-backend:latest .
 docker stack deploy -c brilliantwear-backend-docker-compose-file.yml bw-stack
 
-# Old:
 # To create one container:
 #docker run -p 7000:7000 --add-host=mongoservice:172.17.0.1 -d cscphilip/brilliantwear-backend
