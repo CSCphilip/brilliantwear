da5663c0d7f9b4bb2e4cdbbe1882e9efd6e45e08
Philip <philipandersson27@gmail.com>
2023-10-08 18:19:25 +0200

    Add fullstack-next to start migrating my website to using next.js.

---
+++ fullstack-next/app/components/ProductCard/AddToCart.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button
+        className="btn btn-primary"
+        onClick={() => console.log("Clicked")}
+      >
+        Add to cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
---
+++ fullstack-next/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard/ProductCard";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Next full stack</h1>
+      {/* Use the Link tag instead of <a> since this till only download what's new in the next page 
+      and not download all of the content again which would be the case if using <a>. This is 
+      the next.js/react.js way of doing it and should be used. */}
+      <Link href="/users">UsersPage</Link>
+      <ProductCard />
+    </main>
+  );
+}
+
+/* 
+Next consists of server components which cannot:
+* Listen to browser events
+* Access browser APIs (like local storage)
+* Maintain state
+* Use effects
+*/
