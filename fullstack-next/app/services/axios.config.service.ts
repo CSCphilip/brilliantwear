634e7fe6a614542e832b6d01067081ff7c6b355d
Philip <philipandersson27@gmail.com>
2023-10-22 19:00:11 +0200

    Change name of old tutorial files.

--- fullstack-next/app/services/axios.config.service.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "https://api.brilliantwear.se/",
-  withCredentials: true, //TODO: Later change to true when using authentication through the API
-});
-
-export default axiosInstance;
