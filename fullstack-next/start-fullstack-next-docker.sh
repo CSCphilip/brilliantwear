596f1659f043555c2e794bf8270ae88c7350a494
Philip <philipandersson27@gmail.com>
2023-10-23 18:49:42 +0200

    Change docker related files for the fullstack next.js server.

--- fullstack-next/start-fullstack-next-docker.sh
+++ fullstack-next/start-fullstack-next-docker.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Do not run docker build on the AWS EC2 instance since it will crash the instance
-#docker build -t cscphilip/brilliantwear-fullstack-next:latest .
+# docker build -t cscphilip/brilliantwear-fullstack-next:latest .
 
-docker stack deploy -c fullstack-next-compose.yml next-server-stack
+# docker stack deploy -c fullstack-next-compose.yml bw-next-server-stack
