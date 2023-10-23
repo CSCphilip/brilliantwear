fc94282a52b547ad057b47aa6641f658984af263
Philip <philipandersson27@gmail.com>
2023-10-23 22:01:57 +0200

    Small design change for product image size.

--- fullstack-next/app/components/ProductCard.tsx
+++ fullstack-next/app/components/ProductCard.tsx
@@ -20,7 +20,7 @@ const ProductCard = ({
               encodeURIComponent(image_url)
             }
             alt="An image of the product."
-            className="h-64 w-40 border border-primary"
+            className="h-64 w-[170px] lg:h-72 lg:w-48 border border-primary"
           />
         </Link>
         <ul className="text-sm">
