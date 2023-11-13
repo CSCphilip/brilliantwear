ed4bdd5708b876f90a61d18e14f16107aed4f04a
Philip Andersson <philipandersson27@gmail.com>
2023-11-13 18:04:33 +0000

    Change docker compose file and commands to deploy backend docker containers.

--- backend/start-backend-docker.sh
+++ backend/start-backend-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker build -t cscphilip/brilliantwear-backend:latest .
-docker stack deploy -c brilliantwear-backend-docker-compose-file.yml bw-stack
+docker stack deploy -c compose-backend.yml bw-backend
 
 # To create one container:
 #docker run -p 7000:7000 --add-host=mongoservice:172.17.0.1 -d cscphilip/brilliantwear-backend
